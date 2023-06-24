01ff057f85abf45d6a9972b9fcad5f45533147c9
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-06-24 12:18:09 +0200

    Query that prints a list of employee names

---
+++ basic-sql/name-of-employees.sql
@@ -0,0 +1,3 @@
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order
+-- https://www.hackerrank.com/challenges/name-of-employees/problem
+SELECT name FROM Employee ORDER BY name ASC;
